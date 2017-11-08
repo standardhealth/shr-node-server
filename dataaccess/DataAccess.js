0bf4fcab137151ff31fb0c1260f4ce9bee17147f
Nicole ng <nng@mitre.org>
2017-11-08 13:57:34 -0500

    Added data access folder and modified defaultHandlers to use data access to grab HardCodedPatient

---
+++ dataaccess/DataAccess.js
@@ -0,0 +1,35 @@
+const HardCodedReadOnlyDataSource = require('./HardCodedReadOnlyDataSource');
+const NewPatientOnlyDataSource = require('./NewPatientOnlyDataSource');
+const RestApiDataSource = require('./RestApiDataSource');
+
+export default class DataAccess {
+    static DEMO_PATIENT_ID = "-1";
+    
+    constructor(dataSourceName) {
+        if (dataSourceName === 'HardCodedReadOnlyDataSource') {
+            this.dataSource = new HardCodedReadOnlyDataSource();
+        } else if (dataSourceName === 'NewPatientOnlyDataSource') {
+            this.dataSource = new NewPatientOnlyDataSource();
+        } else if (dataSourceName === 'RestApiDataSource') {
+            this.dataSource = new RestApiDataSource(); 
+        } else { 
+            throw new Error("Unrecognized data source class name: " + dataSourceName);
+        }
+    }
+    
+    getPatient(id) {
+        return this.dataSource.getPatient(id);
+    }
+    
+    getListOfPatients() {
+        return this.dataSource.getListOfPatients();
+    }
+    
+    newPatient() {
+        return this.dataSource.newPatient();
+    }
+    
+    savePatient(patient) {
+        return this.dataSource.savePatient(patient);
+    }
+}
--- fluxImplementation/defaultHandlers.js
+++ fluxImplementation/defaultHandlers.js
@@ -1,27 +1,43 @@
 'use strict'
-const hardCodedPatient = require('./HardCodedPatient.json');
-
-const DEMO_PATIENT_ID = "-1"; 
 
 /**
  * WARNING: If new node.js server code is generated using the swagger.yaml or swagger.json file, you will need to hand edit the
  * generated code. Code to add to controllers/DefaultService.js is:
  * 1. Import this file into the DefaultService.js file. (Ex: const patientHandler = require('../../fluxImplementation/defaultHandlers');)
- * 2. In each function, add the appropriate function from this file to execute the desired outcome, passing in 
+ * 2. In each function, add the appropriate function from this file to execute the desired outcome, passing in
  * all parameters. See each function for the appropriate line to add.
  */
+const DataAccess = require('../dataaccess/DataAccess');
+
+const DEMO_PATIENT_ID = "-1";
+
+const dataSource = 'HardCodedReadOnlyDataSource';
+
+var dataAccess = null;
+
+if (!dataSource) {
+    dataAccess = new DataAccess("HardCodedReadOnlyDataSource");
+} else {
+    dataAccess = new DataAccess(dataSource);
+}
+
+const hardCodedPatient = this.dataAccess.getPatient(DEMO_PATIENT_ID);
+// let patient = this.dataAccess.getPatient(DataAccess.DEMO_PATIENT_ID);
+
+// const hardCodedPatient = require('./HardCodedPatient.json');
+
 
 // In DefaultService.js in getPatientById(), add: patientHandler.getPatientById(args, res, next);
 function getPatientById(args, res, next) {
-  if (args.shrId.value === DEMO_PATIENT_ID) {
-    res.write(JSON.stringify(hardCodedPatient));
-  } else {
-    res.statusCode = 404;
-    res.statusMessage = "Patient not found."
-    res.write("No patient matched the specified ID.");
-  }
+    if (args.shrId.value === DEMO_PATIENT_ID) {
+        res.write(JSON.stringify(hardCodedPatient));
+    } else {
+        res.statusCode = 404;
+        res.statusMessage = "Patient not found."
+        res.write("No patient matched the specified ID.");
+    }
 }
 
 module.exports = {
-  getPatientById
+    getPatientById
 };
